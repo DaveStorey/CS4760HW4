3139c8984014849595c287c10cc147d2dbac0ea0
David Storey <dstorey65@gmail.com>
2019-04-11 12:48:50 -0500

    Message queues working, just PCB to get.

--- child.c
+++ child.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<sys/ipc.h>
+#include<string.h>
 #include<sys/shm.h>
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<sys/msg.h>
+#include<time.h>
 
 struct PCB{
 	unsigned long PCTime, totalTime, lastQuanta;
@@ -12,27 +15,46 @@ struct PCB{
 	pid_t localPid;
 };
 
+struct mesg_buffer{
+	long mesg_type;
+	char mesg_text[100];
+} message;
+
 int main(int argc, char * argv[]){
+	srand(time(0));
 	char * ptr;
+	int msgid;
 	pid_t pid = getpid();
 	unsigned long * shmPTR;
 	struct PCB * pdb;
-	printf("Child %d PDB address: %p \n",getpid(), pdb);
 	unsigned long shmID1;
 	unsigned long key = strtoul(argv[0], &ptr, 10);
 	unsigned long life = strtoul(argv[1], &ptr, 10);
 	unsigned long shmID;
 	unsigned long key1 = strtoul(argv[2], &ptr, 10);
+	unsigned long msgKey = strtoul(argv[3], &ptr, 10);
 	unsigned long existence;
+	msgid = msgget(msgKey, 0666 | IPC_CREAT);
+	msgrcv(msgid, &message, sizeof(message), 1, 0);
 	shmID = shmget(key, sizeof(unsigned long), O_RDONLY);
-	shmPTR = (unsigned long *) shmat(shmID, NULL, 0);
+	shmPTR = (unsigned long *) shmat(shmID, (void *)0, 0);
 	shmID1 = shmget(key1, sizeof(struct PCB[18]), O_RDONLY);
-	pdb = (struct PCB *) shmat(shmID1, NULL, 0);
-	printf("Child %d PDB address: %p \n",getpid(), pdb);
-	existence = shmPTR[0] + life;
-	printf("Shared Memory: %li \n", pdb[0].PCTime);
-	while(shmPTR[0] < existence);
-	printf("Process %d exiting at %li\n", pid, shmPTR[0]);
+	pdb = (struct PCB *) shmat(shmID1, (void *)0, 0);
+	existence = shmPTR[0] + rand() % 500000;
+	while(shmPTR[0] < existence){
+		int blocked = rand() % 100;
+		if (blocked < 25){
+			char messageText[50];
+			sprintf(messageText, "%d", pid);
+			strcat(messageText, " blocked\n");
+			printf("%s\n", messageText);
+			strcpy(message.mesg_text, messageText);
+			msgsnd(msgid, &message, sizeof(message), 0);
+			msgrcv(msgid, &message, sizeof(message), 1, 0);
+			printf("%d running\n", pid);
+		}
+	}
 	shmdt(shmPTR);
+	printf("%d terminating\n", pid);
 	return 0;
 }
--- scheduler1.c
+++ scheduler1.c
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <signal.h>
+#include <sys/msg.h>
 
 //Signal handler to catch ctrl-c
 static volatile int keepRunning = 1;
@@ -22,27 +23,35 @@ struct PCB{
 	pid_t localPid;
 };
 
+struct mesg_buffer{
+	long mesg_type;
+	char mesg_text[50];
+}message;
+
 void scheduler(char* outfile, int limit, int total){
 	/*Variables for the keeping track of pids, a loop variable, waitPID status, how many processes are alive, a flag for if there have been any children spawned, a flag for if the overall total has been reached, a flag for if the time limit has been reached, and a flag for if the instant limit has been reached.*/
-	int i = 0, k, status, totalSpawn = 0, alive = 1, noChildFlag = 1, totalFlag = 0, timeFlag = 0, limitFlag = 0;
+	int i = 0, k, status, totalSpawn = 0, alive = 1, noChildFlag = 1, totalFlag = 0, timeFlag = 0, limitFlag = 0, msgid;
 	//Variables for process nanoseconds, life, shared memory ID, seconds, and the timer increment, respectively.
 	unsigned long quantum = 500000, shmID, shmID1, increment = 0, timeBetween = 0, launchTime = 0;
 	struct PCB * pcbPTR;
 	//Pointer for the shared memory timer
 	unsigned long * shmPTR;
 	//Character pointers for arguments to pass through exec
-	char * parameter[32], parameter1[32], parameter2[32], parameter3[32];
+	char * parameter[32], parameter1[32], parameter2[32], parameter3[32], parameter4[32];
 	pid_t pid[total], endID = 1; 
 	//Time variables for the time out function
 	time_t when, when2;
 	//File pointers for input and output, respectively
 	FILE * outPut;
 	//Key variable for shared memory access.
-	unsigned long key, key1;
+	unsigned long key, key1, msgKey;
 	srand(time(0));
 	timeBetween = (rand() % 300000000);
 	key = rand();
 	key1 = rand();
+	msgKey = rand();
+	msgid = msgget(msgKey, 0666 | IPC_CREAT);
+	message.mesg_type = 1;
 	//Setting initial time for later check.
 	time(&when);
 	outPut = fopen(outfile, "a");
@@ -55,14 +64,11 @@ void scheduler(char* outfile, int limit, int total){
 	
 	//Get and access shared memory, setting initial timer state to 0.
 	shmID = shmget(key, sizeof(unsigned long), IPC_CREAT | IPC_EXCL | 0777);
-	shmPTR = (unsigned long *) shmat(shmID, NULL, 0);
+	shmPTR = (unsigned long *) shmat(shmID, (void *) 0, 0);
 	shmPTR[0] = 0;
 	shmID1 = shmget(key1, sizeof(struct PCB)*18, IPC_CREAT | IPC_EXCL | 0777);
-	pcbPTR = (struct PCB *) shmat(shmID, NULL, 0);
-	for (int z = 0; z < 18; z++){
-		pcbPTR[z].PCTime = 1234;
-	}
-	printf("Parent shared mem: %li, %li \n", pcbPTR[0].PCTime, pcbPTR[1].PCTime);
+	pcbPTR = (struct PCB *) shmat(shmID, (void *) 0, 0);
+	pcbPTR[0].PCTime = 1234;
 	//Initializing pids to -1 
 	for(k = 0; k < total; k++){
 		pid[k] = -1;
@@ -70,7 +76,7 @@ void scheduler(char* outfile, int limit, int total){
 	//Call to signal handler for ctrl-c
 	signal(SIGINT, intHandler);
 	increment = rand() % 5000000;
-	printf("Increment: %ld; quantum: %ld \n", increment, quantum);
+	printf("Parent shmID1: %li; \n", shmID1);
 	//While loop keeps running until all children are dead, ctrl-c, or time is reached.
 	while((alive > 0) && (keepRunning == 1) && (timeFlag == 0)){
 		time(&when2);
@@ -88,11 +94,15 @@ void scheduler(char* outfile, int limit, int total){
 					sprintf(parameter1, "%li", key);
 					sprintf(parameter2, "%li", quantum);
 					sprintf(parameter3, "%li", key1);
-					char * args[] = {parameter1, parameter2, parameter3, NULL};
+					sprintf(parameter4, "%li", msgKey);
+					char * args[] = {parameter1, parameter2, parameter3, parameter4, NULL};
 					execvp("./child\0", args);
 				}
 				else{
 				//If statement to reset alive counter after getting into while loop initially
+					char messageText[] = "I love you!\n", pid[i];
+					strcpy(message.mesg_text, messageText);
+					msgsnd(msgid, &message, sizeof(message), 0);
 					if (noChildFlag > 0){
 						alive--;
 						noChildFlag = 0;
@@ -153,6 +163,9 @@ void scheduler(char* outfile, int limit, int total){
 		fprintf(outPut, "Scheduler terminated at %li nanoseconds due to ctrl-c signal.\n",  shmPTR[0]);
 	}
 	shmdt(shmPTR);
+	shmdt(pcbPTR);
 	shmctl(shmID, IPC_RMID, NULL);
+	shmctl(shmID1, IPC_RMID, NULL);
+	msgctl(msgid, IPC_RMID, NULL);
 	fclose(outPut);
 }
