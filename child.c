e70c9c06c9b9cbfb24cf4cf9cd37bb8c0270f3f9
David Storey <dstorey65@gmail.com>
2019-04-12 16:20:38 -0500

    Blind alley

--- child.c
+++ child.c
@@ -10,14 +10,18 @@
 #include<time.h>
 
 struct PCB{
-	unsigned long PCTime, totalTime, lastQuanta, timeClock;
-	unsigned int priority;
+	unsigned long PCTime, totalTime, lastQuanta, timeClock, created, childKey;
+	unsigned long messageQueue, logical_Num;
 	pid_t localPid;
 };
 
 struct mesg_buffer{
-	long mesg_type;
-	int mesg_data[10];
+	long type;
+	int user_pid;
+	int quanta_used;
+	int priority;
+	int logical;
+	int terminated;
 } message;
 
 int main(int argc, char * argv[]){
@@ -25,33 +29,48 @@ int main(int argc, char * argv[]){
 	char * ptr;
 	int msgid, timeFlag = 0;
 	pid_t pid = getpid();
-	time_t when, when2;
+	int i = 0, blocked = 0;
 	struct PCB * shmPTR;
 	unsigned long shmID;
-	unsigned long key = strtoul(argv[0], &ptr, 10);
-	unsigned long life = strtoul(argv[1], &ptr, 10);
-	unsigned long msgKey = strtoul(argv[2], &ptr, 10);
+	unsigned long key = strtoul(argv[1], &ptr, 10);
+	unsigned long life = strtoul(argv[2], &ptr, 10);
 	unsigned long logicalNum = strtoul(argv[3], &ptr, 10);
-	unsigned long existence;
-	msgid = msgget(msgKey, 0666 | IPC_CREAT);
-	msgrcv(msgid, &message, sizeof(message), 1, 0);
-	shmID = shmget(key, sizeof(struct PCB)*19, O_RDONLY);
+	unsigned long existence = (shmPTR[0].timeClock + (rand() % 250000));
+	//msgid = msgget(sh, 0666 | IPC_CREAT);
+	shmID = shmget(key, sizeof(struct PCB)*19, 0777);
+	printf("Child shmat error, %li\n", shmID);
 	shmPTR = (struct PCB *) shmat(shmID, (void *)0, 0);
-	existence = shmPTR[0].timeClock + rand() % 500000;
+	printf("In child, message queue %li\n", shmPTR[logicalNum].messageQueue);
+	msgrcv(shmPTR[logicalNum].messageQueue, &message, sizeof(message), 1, 0);
+	message.type = 1;
 	while((shmPTR[0].timeClock < existence) && (timeFlag == 0)){
-		int blocked = rand() % 100;
-		if (blocked < 20){
-			char messageText[50];
-			sprintf(messageText, "%d", pid);
-			strcat(messageText, " blocked");
-			printf("%s\n", messageText);
-			strcpy(message.mesg_text, messageText);
-			msgsnd(msgid, &message, sizeof(message), 0);
-			msgrcv(msgid, &message, sizeof(message), 1, 0);
-			printf("%d running\n", pid);
+		if (i > 5){
+			timeFlag = 1;
 		}
+		blocked = rand() % 100;
+		if (blocked < 20){
+			message.quanta_used = (rand() % 99 * shmPTR[logicalNum].lastQuanta);
+			message.user_pid = getpid();
+			message.logical = logicalNum;
+			message.terminated = 0;
+			msgsnd(shmPTR[logicalNum].messageQueue, &message, sizeof(message), 0);
+			shmPTR[logicalNum].PCTime += message.quanta_used;
+			msgrcv(shmPTR[logicalNum].messageQueue, &message, sizeof(message), 1, 0);
+			shmPTR[logicalNum].totalTime = shmPTR[0].timeClock - shmPTR[logicalNum+1].created;
+			printf("%d running after block.\n", pid);
+		}
+		if (blocked >= 20)
+			printf("%d not blocked.\n", pid);
+		i++;
 	}
+	message.terminated = 1;
+	message.type = 1;
+	message.user_pid = pid;
+	msgsnd(shmPTR[logicalNum].messageQueue, &message, sizeof(message), 0);
 	shmdt(shmPTR);
-	printf("%d terminating\n", pid);
+	printf("%d terminating", pid);
+	if (timeFlag)
+		printf(" due to timeflag.");
+	printf("\n");
 	return 0;
 }
--- scheduler2.c
+++ scheduler2.c
@@ -18,8 +18,8 @@ void intHandler(int dummy) {
 }
 
 struct PCB{
-	unsigned long PCTime, totalTime, lastQuanta, timeClock, created;
-	unsigned long priority, logical_Num;
+	unsigned long PCTime, totalTime, lastQuanta, timeClock, created, childKey;
+	unsigned int messageQueue, logical_Num;
 	pid_t localPid;
 };
 
@@ -28,30 +28,37 @@ struct mesg_buffer{
 	int user_pid;
 	int quanta_used;
 	int priority;
+	int terminated;
 }message;
 
 void scheduler(char* outfile, int limit, int total){
 	/*Variables for the keeping track of pids, a loop variable, waitPID status, how many processes are alive, a flag for if there have been any children spawned, a flag for if the overall total has been reached, a flag for if the time limit has been reached, and a flag for if the instant limit has been reached.*/
-	int i = 0, k, status, totalSpawn = 0, alive = 1, noChildFlag = 1, totalFlag = 0, timeFlag = 0, limitFlag = 0, msgid;
+	int i = 0, k, totalSpawn = 0, alive = 1, noChildFlag = 1, totalFlag = 0, timeFlag = 0, limitFlag = 0, msgid, msgid1, msgid2, msgid3;
 	//Variables for process nanoseconds, life, shared memory ID, seconds, and the timer increment, respectively.
 	unsigned long quantum = 500000, shmID, shmID1, increment = 0, timeBetween = 0, launchTime = 0;
 	//Pointer for the shared memory timer
 	struct PCB * shmPTR;
 	//Character pointers for arguments to pass through exec
-	char * parameter[32], parameter1[32], parameter2[32], parameter3[32], parameter4[32];
+	char * parameter[32], parameter1[32], parameter2[32], parameter3[32], parameter4[32], parameter5[32];
 	pid_t pid[total], endID = 1; 
 	//Time variables for the time out function
 	time_t when, when2;
 	//File pointers for input and output, respectively
 	FILE * outPut;
 	//Key variable for shared memory access.
-	unsigned long key, key1, msgKey;
+	unsigned long key, key1, msgKey, msgKey1, msgKey2, msgKey3;
 	srand(time(0));
 	timeBetween = (rand() % 300000000);
 	key = rand();
 	key1 = rand();
 	msgKey = rand();
+	msgKey1 = rand();
+	msgKey2 = rand();
+	msgKey3 = rand();
 	msgid = msgget(msgKey, 0666 | IPC_CREAT);
+	msgid1 = msgget(msgKey1, 0666 | IPC_CREAT);
+	msgid2 = msgget(msgKey2, 0666 | IPC_CREAT);
+	msgid3 = msgget(msgKey3, 0666 | IPC_CREAT);
 	message.type = 1;
 	//Setting initial time for later check.
 	time(&when);
@@ -78,6 +85,7 @@ void scheduler(char* outfile, int limit, int total){
 	signal(SIGINT, intHandler);
 	increment = (rand() + 10000) % 5000000;
 	//While loop keeps running until all children are dead, ctrl-c, or time is reached.
+	printf("Message queues: %d, %d, %d\n", msgid1, msgid2, msgid3);
 	while((alive > 0) && (keepRunning == 1) && (timeFlag == 0)){
 		time(&when2);
 		if ((when2 - when) >= 10){
@@ -90,29 +98,37 @@ void scheduler(char* outfile, int limit, int total){
 		//If statement to spawn child if timer has passed its birth time.
 			if(shmPTR[0].timeClock >= (launchTime + timeBetween)){
 				if((pid[i] = fork()) == 0){
+					int level = rand() % 10;
 				//Converting key, shmID and life to char* for passing to exec.
 					sprintf(parameter1, "%li", key);
 					sprintf(parameter2, "%li", quantum);
-					sprintf(parameter3, "%li", msgKey);
-					sprintf(parameter4, "%d", i+1);
-					int level = rand() % 10;
-					if (level <=1)
-						shmPTR[i+1].priority = 0;
-					else{
-						shmPTR[i+1].priority = 1;
+					sprintf(parameter3, "%d", i+1);
+					printf("Level = %d\n", level);
+					if (level <=1){
+						shmPTR[i+1].messageQueue = msgid1;
+						shmPTR[i+1].childKey = msgKey1;
 					}
-					shmPTR[i+1].logicalNum = i+1;
+					else if (level <=6){
+						shmPTR[i+1].messageQueue = msgid2;
+						shmPTR[i+1].childKey = msgKey2;
+					}
+					else{
+						shmPTR[i+1].messageQueue = msgid3;
+						shmPTR[i+1].childKey = msgKey3;
+					}
+					shmPTR[i+1].logical_Num = i+1;
 					shmPTR[i+1].lastQuanta = 50000;
 					shmPTR[i+1].PCTime = 0;
 					shmPTR[i+1].totalTime = 0;
 					shmPTR[i+1].created = shmPTR[0].timeClock;
-					char * args[] = {parameter1, parameter2, parameter3, parameter4, NULL};
-					printf("Launching child %d\n", getpid());
+					char * args[] = {parameter1, parameter2, parameter3, NULL};
+					printf("Launching child %d in message queue %d\n", getpid(), shmPTR[i+1].messageQueue);
 					execvp("./child\0", args);
 				}
 				else{
 				//If statement to reset alive counter after getting into while loop initially
-					msgsnd(msgid, &message, sizeof(message), 0);
+					
+							
 					if (noChildFlag > 0){
 						alive--;
 						noChildFlag = 0;
@@ -125,22 +141,29 @@ void scheduler(char* outfile, int limit, int total){
 				}
 			}
 		}
-		//EndID to check for status of group children
-		endID = waitpid(0, &status, WNOHANG | WUNTRACED);
-		if (endID == -1){
-		//	perror("waitpid error");
+		for (k = 1; k <= alive; k++){
+			if (shmPTR[k].messageQueue == msgid1){
+				msgsnd(msgid1, &message, sizeof(message), 0);
+				msgrcv(msgid1, &message, sizeof(message), 1, 0);
+			}
 		}
-		else if (endID == 0);
-		else{
-		//For loop steps through array of pids, checking if return value matches.
-			for(k = 0; k <= i; k++){
-				if(endID == pid[k]){
-					alive--;
-					limitFlag = 0;
-				}
+		for (k = 1; k <= alive; k++){
+			if (shmPTR[k].messageQueue == msgid2){
+				msgsnd(msgid2, &message, sizeof(message), 0);
+				msgrcv(msgid2, &message, sizeof(message), 1, 0);
+			}
+		}
+		for (k = 1; k <= alive; k++){
+			if (shmPTR[k].messageQueue == msgid3){
+				msgsnd(msgid3, &message, sizeof(message), 0);
+				msgrcv(msgid3, &message, sizeof(message), 1, 0);
 			}
 		}
 		//If statements check against total children spawned and instant limit.
+		if (message.terminated == 1){
+			limitFlag = 0;
+			alive--;
+		}
 		if (totalSpawn >= total){
 			totalFlag = 1;
 		}
@@ -157,8 +180,8 @@ void scheduler(char* outfile, int limit, int total){
 		fprintf(outPut, "Scheduler terminated at %li nanoseconds due to process limit.\n",  shmPTR[0].timeClock);
 	}
 	if(keepRunning == 0){
-		printf("Terminated due to ctrl-c signal.\n");
-		fprintf(outPut, "Scheduler terminated at %li nanoseconds due to ctrl-c signal.\n",  shmPTR[0].timeClock);
+		printf("Terminated due to ctrl-c signal at %li with %d children.\n", shmPTR[0].timeClock, alive);
+		fprintf(outPut, "Scheduler terminated at %li nanoseconds due to ctrl-c signal with %d children.\n", shmPTR[0].timeClock, alive);
 	}
 	if(alive > 0){
 	int j = 0;
@@ -171,5 +194,8 @@ void scheduler(char* outfile, int limit, int total){
 	shmdt(shmPTR);
 	shmctl(shmID, IPC_RMID, NULL);
 	msgctl(msgid, IPC_RMID, NULL);
+	msgctl(msgid1, IPC_RMID, NULL);
+	msgctl(msgid2, IPC_RMID, NULL);
+	msgctl(msgid3, IPC_RMID, NULL);
 	fclose(outPut);
 }
